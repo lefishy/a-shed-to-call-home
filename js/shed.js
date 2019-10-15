3c31c7e0cb2ea9d4bc8222f3fb77abc787a84b45
Luke <lefishy@gmail.com>
2019-10-15 21:01:01 +0100

    camera fiddling also a shed object

--- js/shed.js
+++ js/shed.js
@@ -1,21 +1,25 @@
 var scene = new THREE.Scene();
 
 var clearHue = THREE.Math.randInt(0,360);
-scene.background = new THREE.Color("hsl("+clearHue+",25%,80%)");
+scene.background = new THREE.Color("hsl("+clearHue+",25%,90%)");
 var camera = new THREE.PerspectiveCamera(75,window.innerWidth/window.innerHeight, 0.1, 1000);
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth,window.innerHeight);
 
 document.body.appendChild(renderer.domElement);
 
-var groundGeo = new THREE.CircleGeometry(5,32);
+var shed = new THREE.Group();
+
+scene.add(shed);
+
+var groundGeo = new THREE.CircleGeometry(10,32);
 var groundMat = new THREE.MeshBasicMaterial({color:0x00ff00});
 var ground = new THREE.Mesh(groundGeo, groundMat);
 
-scene.add(ground);
+shed.add(ground);
 
-camera.position.z = 8;
-camera.position.y = -15;
+camera.position.z = 15;
+camera.position.y = -18;
 
 camera.rotation.x = 1;
 
