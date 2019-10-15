835567198e5141ff83b2be2ba93205e2aca8687f
lefishy <lefishy@gmail.com>
2019-10-15 16:34:37 +0100

    that definitely wasn't going to work

--- js/shed.js
+++ js/shed.js
@@ -1,7 +1,8 @@
 var scene = new THREE.Scene();
+
+var clearHue = THREE.Math.randInt(0,360);
 scene.background = new THREE.Color("hsl("+clearHue+",55%,80%)");
 var camera = new THREE.PerspectiveCamera(75,window.innerWidth/window.innerHeight, 0.1, 1000);
-var clearHue = THREE.Math.randInt(0,360);
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth,window.innerHeight);
 
